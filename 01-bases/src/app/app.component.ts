f359ad562a98adc330254a4fa23cd98d801671ee
BeJuanitox <jsebastian23@hotmail.com>
2023-02-01 15:01:29 -0500

    Accumulate App

--- 01-bases/src/app/app.component.ts
+++ 01-bases/src/app/app.component.ts
@@ -6,5 +6,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'bases';
+  title: string = 'Contador App';
+  number: number = 10;
+  base: number = 5;
+
+  /*add () {
+    this.number += 1;
+  }
+
+  substract () {
+    this.number -= 1;
+  }*/
+  
+  accumulate (vlr: number) {
+    this.number += vlr;
+  }
+
 }
