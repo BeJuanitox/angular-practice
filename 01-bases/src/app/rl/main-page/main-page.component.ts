f900b7e68449a9f2fc7b375eca2aa76aba894929
BeJuanitox <jsebastian23@hotmail.com>
2023-02-04 13:09:38 -0500

    ngModule

--- 01-bases/src/app/rl/main-page/main-page.component.ts
+++ 01-bases/src/app/rl/main-page/main-page.component.ts
@@ -1,13 +1,28 @@
 import { Component } from '@angular/core';
 
+interface Car {
+  name: string;
+  iq: number;
+}
+
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
   styleUrls: ['./main-page.component.css']
 })
 export class MainPageComponent {
+  
+  new:Car = {
+    name: 'Scarab',
+    iq: 90000
+  }
+
+  changeName(e: any) {
+    console.log(e)
+  }
+  
   add() {
-    console.log('Hablalo');
+    console.log(this.new);
   }
 }
 
