53e1cd511c8fe0fc3fcb58492afb2f4ddcdd8747
BeJuanitox <jsebastian23@hotmail.com>
2023-02-05 12:22:50 -0500

    Create new component

---
+++ 01-bases/src/app/rl/cars/cars.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cars',
+  templateUrl: './cars.component.html'
+})
+export class CarsComponent   {
+
+  cars: any[] = [];
+
+}
--- 01-bases/src/app/rl/main-page/main-page.component.ts
+++ 01-bases/src/app/rl/main-page/main-page.component.ts
@@ -8,7 +8,6 @@ interface Car {
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
-  styleUrls: ['./main-page.component.css']
 })
 export class MainPageComponent {
 
