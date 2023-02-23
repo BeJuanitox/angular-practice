76f10815b17b8ad683654b67e6ae5c89af37ddd3
BeJuanitox <jsebastian23@hotmail.com>
2023-02-23 15:21:20 -0500

    API Key Giphy

--- 02-someGifs/src/app/gifs/services/gif.service.ts
+++ 02-someGifs/src/app/gifs/services/gif.service.ts
@@ -4,6 +4,8 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class GifService {
+
+  private apiKey : string = '21mILHlQddwmCVGBjUALb579ctTjiInp';
   private _history: string[] = [];
 
   get history() {
