ef815e2c124aba1036e417dd4da5594bec9dd001
BeJuanitox <jsebastian23@hotmail.com>
2023-03-07 17:37:48 -0500

    Component sidebar

--- 03-countriesApp/src/app/shared/shared.module.ts
+++ 03-countriesApp/src/app/shared/shared.module.ts
@@ -1,6 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
 import { SidebarComponent } from './sidebar/sidebar.component';
+
+import { RouterModule } from '@angular/router';
 
 
 @NgModule({
@@ -11,7 +14,8 @@ import { SidebarComponent } from './sidebar/sidebar.component';
     SidebarComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ]
 })
 export class SharedModule { }
