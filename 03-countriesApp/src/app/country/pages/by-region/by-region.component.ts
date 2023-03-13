7e1493b49ba69d9c4598ccd2b94261605332ad50
BeJuanitox <jsebastian23@hotmail.com>
2023-03-12 19:44:32 -0500

    CSS conditional class

--- 03-countriesApp/src/app/country/pages/by-region/by-region.component.ts
+++ 03-countriesApp/src/app/country/pages/by-region/by-region.component.ts
@@ -12,11 +12,15 @@ import { Component } from '@angular/core';
 })
 export class ByRegionComponent {
 
-  regions: string[] = ['africa', 'americas', 'asia', 'europe', 'oceania']
+  regions: string[] = ['EU', 'EFTA', 'CARICOM', 'PA', 'AU', 'USAN', 'EEU', 'AL', 'ASEAN', 'CAIS', 'CEFTA', 'NAFTA', 'SAARC']
   activeRegion: string = '';
-
+  //regionalbloc
   constructor() { }
   
+  getCSSClass(region: string) {
+    return  (region === this.activeRegion) ? 'btn btn-primary': 'btn btn-outline-primary';
+  }
+
   activateRegion(region: string) {
     this.activeRegion = region;
   }
