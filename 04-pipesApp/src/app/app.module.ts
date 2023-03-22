362ce187b0dfa4a3d8139b28e6b8bc9b544d313d
BeJuanitox <jsebastian23@hotmail.com>
2023-03-22 16:32:36 -0500

    PrimeNg module

--- 04-pipesApp/src/app/app.module.ts
+++ 04-pipesApp/src/app/app.module.ts
@@ -1,10 +1,9 @@
+import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 
-import { ButtonModule } from 'primeng/button';
-import { CardModule } from 'primeng/card';
+import { PrimeNgModule } from './prime-ng/prime-ng.module';
 
 @NgModule({
   declarations: [
@@ -12,8 +11,7 @@ import { CardModule } from 'primeng/card';
   ],
   imports: [
     BrowserModule,
-    ButtonModule,
-    CardModule
+    PrimeNgModule
   ],
   providers: [],
   bootstrap: [AppComponent]
