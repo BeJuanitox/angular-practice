a0ec30fd9749363da712d89281b4e2e06c0d3a98
BeJuanitox <jsebastian23@hotmail.com>
2023-04-25 18:48:33 -0500

    Add all pages needed

---
+++ 06-heroesApp/src/app/shared/pages/err404-page/err404-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-err404-page',
+  templateUrl: './err404-page.component.html',
+  styles: [
+  ]
+})
+export class Err404PageComponent {
+
+}
---
+++ 06-heroesApp/src/app/shared/shared.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { Err404PageComponent } from './pages/err404-page/err404-page.component';
+
+
+@NgModule({
+  declarations: [
+    Err404PageComponent
+  ],
+  exports: [
+    Err404PageComponent
+  ]
+})
+export class SharedModule { }
