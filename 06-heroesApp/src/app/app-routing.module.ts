8b054a9f046e710e9f3ab96808b6c481e5af0997
BeJuanitox <jsebastian23@hotmail.com>
2023-04-25 19:23:20 -0500

    Add routes children and lazyload

--- 06-heroesApp/src/app/app-routing.module.ts
+++ 06-heroesApp/src/app/app-routing.module.ts
@@ -1,7 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Err404PageComponent } from './shared/pages/err404-page/err404-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'auth',
+    loadChildren: () => import('./auth/auth.module').then( m => m.AuthModule )
+  },
+  {
+    path: 'heroes',
+    loadChildren: () => import('./heroes/heroes.module').then( m => m.HeroesModule )
+  },
+  {
+    path: '404',
+    component: Err404PageComponent
+  },
+  {
+    path: '',
+    redirectTo: 'heroes',
+    pathMatch: 'full'
+  },
+  {
+    path: '**',
+    redirectTo: '404'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
