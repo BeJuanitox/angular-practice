af28746b84189f4ac82c9222ae637e4d2f41ad41
BeJuanitox <jsebastian23@hotmail.com>
2023-05-19 17:30:01 -0500

    Get heroe ID

--- 06-heroesApp/src/app/heroes/services/heroes.service.ts
+++ 06-heroesApp/src/app/heroes/services/heroes.service.ts
@@ -1,7 +1,8 @@
 
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, catchError, of } from 'rxjs';
+
 import { Hero } from '../interfaces/hero.interface';
 import { environments } from 'src/environments/environments';
 
@@ -15,6 +16,13 @@ export class HeroesService {
 
   getHeroes(): Observable<Hero[]> {
     return this.http.get<Hero[]>(`${this.baseURL}/heroes`);
+  }
+
+  getHeroById( id: string ): Observable<Hero|undefined> {
+    return this.http.get<Hero>(`${ this.baseURL }/heroes/${ id }`)
+      .pipe(
+        catchError( error => of(undefined) )
+      );
   }
 
 }
